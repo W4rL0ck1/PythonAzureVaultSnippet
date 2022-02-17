df0823b3d39ce9e94e992b3be7c8394c8613f988
Renato Santos <renato_boy3@hotmail.com>
2022-02-17 15:15:33 -0300

    uploading readme and using PYODBC as ORM

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
+import pyodbc
+
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential, AzureCliCredential
 
+#Getting the vault key value to the retrieved_secret variable
 keyVaultName = getenv('KEY_VAULT_NAME')
 KVUri = f"https://{keyVaultName}.vault.azure.net"
 
@@ -8,9 +15,16 @@ KVUri = f"https://{keyVaultName}.vault.azure.net"
 #credential = DefaultAzureCredential()
 credential = AzureCliCredential()
 client = SecretClient(vault_url=KVUri, credential=credential)
-
 secretName = "ConnectionStrings--HealthCheckAdminDb"
 
 retrieved_secret = client.get_secret(secretName)
+#print(f"The secret value is '{retrieved_secret.value}'.")
 
-print(f"The secret value is '{retrieved_secret.value}'.")
+
+##Vault Connection, getting the key, adding some treatments and instantiating the PYOBDC cursor object to do the queries with it.
+key = retrieved_secret.value
+# Replacing the values names to the names recognized by the ODBC DRIVER
+key = key.replace("Initial Catalog","DATABASE").replace("User ID","UID").replace("Password", "PWD").replace("Encrypt=True;TrustServerCertificate=False;", "")
+connection = 'DRIVER={ODBC Driver 17 for SQL Server};'+ key
+cnxn = pyodbc.connect(connection)
+cursor = cnxn.cursor()
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+
 # PythonAzureVaultSnippet
+
+  
+
+Snippet to use Azure Vault keys with PYODBC to do the queries.
+
+  
+
+# 1. Install the packages.
+## Install the Azure Active Directory identity library:
+
+    pip install azure-identity
+
+## Install the Key Vault secrets library:
+
+    pip install azure-keyvault-secrets
+
+# 2. Set your vault key name on the *keyVaultName* variable
+
+# 3. Run your code.
